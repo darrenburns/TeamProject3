6f9a4c756779475577d7ce8b22dcb05a9d628eaa
Darren Burns <darrenb900@gmail.com>
2014-10-28 01:25:39 +0000

    Page with form rendered

--- src/snptm/urls.py
+++ src/snptm/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
     url(r'^accounts/login/', 'core.views.user_login', name='user_login'),
     url(r'^accounts/logout/', 'core.views.user_logout', name='user_logout'),
+    url(r'^accounts/register/$', 'core.views.user_register', name='user_register'),
 )
