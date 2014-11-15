e5a794287df9c1b4f016abdb4c7f1cedbacf558e
Tom Wallis <tom@tom-wallis.com>
2014-11-15 18:31:13 +0000

    Refactored the models a little.

--- chat/models.py
+++ chat/models.py
@@ -16,3 +16,15 @@ class Message(models.Model):
     project = models.ForeignKey(Chat) # The chat this message is contained in.
     user = models.ForeignKey(User) # The user who created the message.
     sent = models.DateTimeField() # The date and time the message was sent at.
+
+
+class Priority(models.Model):
+    name = models.CharField(max_length = 20) # The name of a priority.
+    colour = models.CharField(max_length = 20, primary_key = True) # The name of the priority, which is unique.
+
+
+class Ticket(models.Model):
+    notes = models.CharField(max_length = 500) # Notes on a chat.
+    created = models.DateTimeField() # When a ticket relating to a chat was created.
+    closed = models.DateTimeField() # If closed, when a ticket was closed.
+    priority = models.ForeignKey(Priority) # The priority of a ticket.
