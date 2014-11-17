adc63d24fc3120653f712c0534ecfa9a0b216fb5
Darren Burns <darrenb900@gmail.com>
2014-11-17 13:59:20 +0000

    Cleaned up models and fixed imports

--- chat/models.py
+++ chat/models.py
@@ -1,30 +1,26 @@
 from django.db import models
-import os
-os.path.join(os.getcwd(), '../') # A hack to get these modules to god damn import. Please, somebody, save me from this and find out how it can be cleaned up. Python imports are horrific.
-from core.models import *
-os.path.join(os.getcwd(), 'chat/') # Undo the work we did previously.
 
 
 class Chat(models.Model):
-    project = models.ForeignKey(Project) # The project this chat belongs to.
-    created = models.DateTimeField() # Date the chat was created in.
-    closed = models.DateTimeField() # If it exists, the date the chat was closed in.
-    user = models.ManyToManyField(User)
+    project = models.ForeignKey('core.Project')
+    created = models.DateTimeField()
+    closed = models.DateTimeField(null=True)
+    user = models.ManyToManyField('core.User')
 
 
 class Message(models.Model):
-    project = models.ForeignKey(Chat) # The chat this message is contained in.
-    user = models.ForeignKey(User) # The user who created the message.
-    sent = models.DateTimeField() # The date and time the message was sent at.
+    project = models.ForeignKey('chat.Chat')
+    user = models.ForeignKey('core.User')
+    sent = models.DateTimeField()
 
 
 class Priority(models.Model):
-    name = models.CharField(max_length = 20) # The name of a priority.
-    colour = models.CharField(max_length = 20, primary_key = True) # The name of the priority, which is unique.
+    name = models.CharField(max_length=20)  # e.g. High, Normal, Low
+    colour = models.CharField(max_length=20)
 
 
 class Ticket(models.Model):
-    notes = models.CharField(max_length = 500) # Notes on a chat.
-    created = models.DateTimeField() # When a ticket relating to a chat was created.
-    closed = models.DateTimeField() # If closed, when a ticket was closed.
-    priority = models.ForeignKey(Priority) # The priority of a ticket.
+    notes = models.CharField(max_length=500)
+    created = models.DateTimeField()
+    closed = models.DateTimeField(null=True)
+    priority = models.ForeignKey(Priority)
--- core/models.py
+++ core/models.py
@@ -1,36 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User as djangoUser
 
-'''
-
-    TODO: Decide whether we want to have the date fields here as models.DateField
-            ...or models.DateTimeField instances.
-
-    TODO: We need to fix the problem we have below, where Users have Companies as foreign keys and Companies have Users as Foreign Keys.
-            ...the way Django does this is such that we reference the class in a function call. 
-            ...how are we supposed to do this if one is defined before the other?
-            ..see lines 58, 68. 
-
-    Discussion points:
-        Should user roles be CharFields?
-
-    NOTE: Users are made using Django's built in user models, and Permissions are managed using Permissions and Groups.
-'''
-
 
 class UserProfile(models.Model):
-    role = models.CharField(max_length = 50) # A user's role. Should this be a CharField?
-    linkedinURL = models.URLField(max_length = 50) # A user's Linkedin Profile.
-    # image = models.ImageField(upload_to=None) # A User's Profile Picture
+    role = models.CharField(max_length=50)
+    linkedinURL = models.URLField(max_length=50)
+    #image = models.ImageField(upload_to=None)
 
 
 class User(djangoUser):
-    profile = models.OneToOneField(UserProfile) # The user profile.
-    permission = models.ManyToManyField(Permission) # Django will create a new table for us using this.
-    #chat = models.ManyToManyField(Chat) # The chats the user belongs to. (Django will create a new table for this.)
+    profile = models.OneToOneField(UserProfile)
 
 
-# Create your models here.
 class Project(models.Model):
-    manager = models.ForeignKey(User) # A name. This should take permissions into account, but @darrenburns may need to advise on the direction to take here...
-    desc = models.CharField(max_length = 500) # A description. We allow for length here.
+    manager = models.ForeignKey(User)
+    desc = models.CharField(max_length=500)
