80dc52647d867200523316981eee3830c1fcc829
Darren Burns <darrenb900@gmail.com>
2014-11-22 16:34:15 +0000

    Add chat to api

--- core/api/resources.py
+++ core/api/resources.py
@@ -1,4 +1,5 @@
 # REST API for the core models are defined here
+from tastypie.constants import ALL
 
 from tastypie.resources import ModelResource
 from core.models import Project
@@ -7,5 +8,9 @@ from core.models import Project
 class ProjectResource(ModelResource):
 
     class Meta:
+        resource_name = 'project'
         queryset = Project.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            'id': ALL,
+        }
--- snptm/urls.py
+++ snptm/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from tastypie.api import Api
+from chat.api.resources import ChatResource
 from core.api.resources import ProjectResource
 
 # Enable the admin interface
@@ -9,7 +10,7 @@ admin.autodiscover()
 # Register the API
 v1_api = Api(api_name='v1')
 v1_api.register(ProjectResource())
-
+v1_api.register(ChatResource())
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
