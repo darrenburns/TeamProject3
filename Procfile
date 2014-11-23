8961ee482a6cd1e955bdfd003bed80f2ba589ee2
Darren <darrenb900@gmail.com>
2014-11-23 01:28:46 +0000

    Attempt at static files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn snptm.wsgi --log-file -
+web: python manage.py collectstatic --noinput; gunicorn snptm.wsgi --log-file -
--- snptm/settings.py
+++ snptm/settings.py
@@ -94,6 +94,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 # For static files that aren't tied to a particular app
