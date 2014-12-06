bd0a3c4b9c65e1a75b23fda2bf3013b7bda278b2
Leonardo Linhares <slinharesleo@gmail.com>
2014-12-06 16:51:13 +0000

    Test

---
+++ chat/static/chat/js/chat.js
@@ -0,0 +1,75 @@
+$(function () {
+    if (typeof CHAT_ID != 'undefined') {
+        // Initialise the Firebase
+        var ref = new Firebase("https://torid-fire-4899.firebaseio.com/");
+
+        // Creating a chat object
+        var chatObj = ref.child('chats/' + CHAT_ID);
+
+        //creating the child note participants to the chat
+        var chatParticipants = chatObj.child("participants");
+
+        var messagesRef = chatObj.child("messages");
+
+
+        // Select the input box and the latest messages list
+        var messages = $(".latest-messages");
+        var messageInput = $("#input-message");
+
+        messageInput.keypress(function (e) {
+            if (e.keyCode == 13) {
+                messagesRef.push({
+                    desc: messageInput.val(),
+                    user: CURRENT_USER,
+                    dt: Date.now()
+                });
+
+                messageInput.val("");
+            }
+        });
+
+        messagesRef.on("child_added", function (object) {
+
+            addMessage(object)
+
+        });
+
+        function addMessage(object) {
+            var child = object.val();
+            chatParticipants.push(CURRENT_USER);
+
+            //Create a new date field to use in Mustache
+            child.formattedDate = getFormattedDate(child.dt);
+
+            var messagesTemplate =
+                '<div class="list-group">' +
+                    '<a href="#" class="list-group-item active">'+
+                        '<h4 class="list-group-item-heading">List group item heading</h4>'+
+                        '<p class="list-group-item-text">'+
+
+            '<li class="list-group-item">' +
+            '<strong> {{ formattedDate }} </strong>' +
+            '<br>' +
+            '<strong>{{ user }}:</strong> {{ desc }} ' +
+            '</li>'
+                        '</p>'+
+                    '</a>'+
+                '</div>'
+
+            var renderedTemplate = Mustache.to_html(messagesTemplate, child);
+            messages.append(renderedTemplate);
+
+
+            // On new message load, scroll to the top.
+            messages[0].scrollTop = messages[0].scrollHeight;
+        }
+
+        //Format the date into day/month/year format
+        function getFormattedDate(dt) {
+            var date = new Date(dt);
+            return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+        }
+    }
+
+
+});
