e8324c3327a94b5277c22d47ad058ebd9961983d
Leonardo Linhares <slinharesleo@gmail.com>
2014-12-14 16:24:24 +0000

    chat with project title subheading

--- chat/views.py
+++ chat/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from chat.models import Chat
 
-
 @login_required()
 def chat(request, chat_id):
     chat = Chat.objects.get(id=chat_id)
-    return render(request, 'chat.html', {'chat': chat})
+    return render(request, 'chat.html', {'chat':chat})
