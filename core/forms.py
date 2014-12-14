ba3718786fa32620ecf3615671af3a4f448b319e
Tom Wallis <tom@tom-wallis.com>
2014-12-14 15:29:26 +0000

    Cleaned up forms, and worked out staff/superuser toggling on account creation. Decision on integrating this to be made and committed soon.

--- core/forms.py
+++ core/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import Group, User
 
 # Helper function to avoid list comprehensions.
 def usersInGroup(groupName):
-	group = Group.objects.filter(name=groupName)[0]
-	usersToReturn = []
-	for user in User.objects.all():
-		if group in user.groups.all():
-			usersToReturn.append(user)
+    group = Group.objects.filter(name=groupName)[0]
+    usersToReturn = []
+    for user in User.objects.all():
+        if group in user.groups.all():
+            usersToReturn.append(user)
 	return usersToReturn
 
 
@@ -17,9 +17,14 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
+        toBeProjectManager = len(usersInGroup("project manager")) == 0
+#        SHOULD PROJECT MANAGERS BE STAFF/SUPERUSERS?
+#        if toBeProjectManager:
+#            user.is_staff = True
+#            user.is_superuser = True
         if commit:
             user.save()
-            if len(usersInGroup("project manager")) == 0:
+            if toBeProjectManager: 
                 user.groups.add(Group.objects.filter(name="project manager")[0])
             else:
                 user.groups.add(Group.objects.filter(name="developer")[0])
