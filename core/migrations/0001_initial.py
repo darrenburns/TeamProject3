ad40deb69e18fb6b3be8da99a910cfb7dc61b036
Darren Burns <darrenb900@gmail.com>
2015-01-15 16:13:39 +0000

    Dropped all migrations

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -16,10 +16,14 @@ class Migration(migrations.Migration):
             name='Project',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('desc', models.CharField(max_length=500)),
+                ('name', models.CharField(max_length=32)),
+                ('desc', models.CharField(max_length=255)),
+                ('homepage', models.URLField(max_length=255, null=True, blank=True)),
+                ('repo', models.URLField(max_length=255, null=True, blank=True)),
                 ('manager', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'permissions': (('canManageProjects', 'Is allowed to manage a project'),),
             },
             bases=(models.Model,),
         ),
@@ -27,8 +31,8 @@ class Migration(migrations.Migration):
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('role', models.CharField(max_length=50)),
-                ('linkedinURL', models.URLField(max_length=50)),
+                ('role', models.CharField(max_length=50, null=True, blank=True)),
+                ('linkedinURL', models.URLField(max_length=255, null=True, blank=True)),
                 ('user', models.OneToOneField(null=True, to=settings.AUTH_USER_MODEL)),
             ],
             options={
