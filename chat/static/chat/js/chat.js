680f0f2a799a071ed508f393758d312299951ccf
Darren Burns <darrenb900@gmail.com>
2015-01-27 17:32:04 +0000

    Change firebase so that it indexes with project id then chat and also add initial scaffolding for the visualisations

--- chat/static/chat/js/chat.js
+++ chat/static/chat/js/chat.js
@@ -69,12 +69,13 @@ $(function () {
     //Add category of metadata
 
 
-    if (typeof CHAT_ID != 'undefined') {
+    if (typeof CHAT_ID != 'undefined' && typeof PROJECT_ID != 'undefined') {
         // Initialise the Firebase
         var ref = new Firebase("https://torid-fire-4899.firebaseio.com/");
 
         // Creating a chat object
-        var chatObj = ref.child('chats/' + CHAT_ID);
+        var projectObj = ref.child('project/' + PROJECT_ID);
+        var chatObj = projectObj.child('chats/' + CHAT_ID);
 
         //creating the child note participants to the chat
         var chatParticipants = chatObj.child("participants");
---
+++ core/static/core/js/project_visualise.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    d3.select("#d3-graph-one").append("p").text("D3 stuffs!");
+
+    var ref = new Firebase("https://torid-fire-4899.firebaseio.com/");
+
+
+
+
+});
