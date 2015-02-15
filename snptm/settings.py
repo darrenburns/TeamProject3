731b60dca3b1deaebebb6d998bed66a331332fa4
Darren Burns <darrenb900@gmail.com>
2015-02-15 14:12:54 +0000

    debugging broken live

--- snptm/settings.py
+++ snptm/settings.py
@@ -66,19 +66,16 @@ WSGI_APPLICATION = 'snptm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 if is_production():
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
-    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
