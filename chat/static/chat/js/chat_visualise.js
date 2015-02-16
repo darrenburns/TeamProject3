47133ab0e87c91c36cbf17d2ff9b7671f6896994
Tom Wallis <tom@tom-wallis.com>
2015-02-16 13:47:36 +0000

    Initial working visualisation for messages in chat by user

--- chat/static/chat/js/chat_visualise.js
+++ chat/static/chat/js/chat_visualise.js
@@ -2,6 +2,12 @@ $(function() {
 
     if (typeof CHAT_ID != 'undefined' && typeof PROJECT_ID != 'undefined') {
 
+        var BASE_CHART_WIDTH = 400;
+        var BASE_CHART_HEIGHT = 250;
+        var BASE_PADDING_LEFT = 30;
+        var BASE_PADDING_TOP = 30;
+        var BASE_PADDING_BOTTOM = 30;
+
         // Initialise the Firebase
         var ref = new Firebase("https://torid-fire-4899.firebaseio.com/");
 
@@ -12,13 +18,66 @@ $(function() {
         var messagesRef = chatObj.child("messages");
 
         // One day, this will be made to work! Maybe today?
-        /*messagesRef.once('value', function(snap) {
+        chatObj.once('value', function(snap) {
 
-            for (var message in snap) {
-                console.log(snap);
+            // Get the messages and count who they're from.
+            // TODO: There's a more elegant way to deal with these keys, to be cleaned up.
+            var messages = snap.val().messages;
+            var userCount = {};
+            for (var messageKey in messages) {
+                message = messages[messageKey];
+                if (message.hasOwnProperty('user')) {
+                    if (message.user in userCount) {
+                        userCount[message.user]++;
+                    } else {
+                        userCount[message.user] = 1;
+                    }
+                }
             }
 
-        })*/
+            var users = Object.keys(userCount);
+            var messageCountPerUser = Object.keys(userCount).map(function (key) {
+                    return userCount[key];
+                });
+
+            // The svg to contain the graph!
+            var svg = d3.select('.chat-visualisations').append('svg')
+                .attr("width", BASE_CHART_WIDTH)
+                .attr("height", BASE_CHART_HEIGHT + BASE_PADDING_BOTTOM + BASE_PADDING_TOP);
+
+            // Create the scale
+            var scale = d3.scale.linear()
+                .domain([0, Math.max.apply(null, messageCountPerUser)])
+                .range([BASE_CHART_HEIGHT, 0])
+                .clamp(true);
+
+            var yAxis = d3.svg.axis()
+                .scale(scale)
+                .orient("left")
+                .ticks(4);
+
+            svg.selectAll("rect")
+                .data(messageCountPerUser)
+                .enter()
+                .append("rect")
+                .attr("x", function(val, idx) {
+                    return BASE_PADDING_LEFT + idx * ((BASE_CHART_WIDTH - BASE_PADDING_LEFT) / messageCountPerUser.length);  // ensure that the bars scale to fit the svg
+                })
+                .attr("y", function(val) {
+                    return BASE_CHART_HEIGHT - scale(val) + BASE_PADDING_TOP;
+                })
+                .attr("width", (BASE_CHART_WIDTH - BASE_PADDING_LEFT) / messageCountPerUser.length)
+                .attr("height", function(val) {
+                    return scale(val);
+                })
+                .attr("fill", "lightblue");
+
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + BASE_PADDING_LEFT + "," + BASE_PADDING_TOP +")")
+                .call(yAxis);
+            })
 
     }
+
 })
