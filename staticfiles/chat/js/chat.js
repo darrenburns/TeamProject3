fbeceb528ff897f89ef369e5b1a03775b399858a
Tom Wallis <tom@tom-wallis.com>
2015-02-24 14:40:33 +0000

    Not working, but began the visualisations on the user page.

---
+++ staticfiles/chat/js/chat.js
@@ -0,0 +1,273 @@
+$(function () {
+
+    //Cache the container and the input for later use
+    var messages = $(".latest-messages");
+    var messageInput = $("#input-message");
+    var tabInformation = $("#tab-information");
+    var selectMetadataName;
+    var apiCall = "/api/v1/";
+    
+    //This function will update the max-height of the container to adapt to different screens
+    //It is done by calculating the difference between the height of the window and the HTML elements
+    //outerHeight is the height of element with its margins
+    function setContainerHeight() {
+        var windowHeight = $( window ).outerHeight();
+        var navbarHeight = $(".navbar-inverse").outerHeight();
+        var chatTitleHeight = $("#chat-title").outerHeight();
+        var navTabsHeight = $(".nav-tabs").outerHeight();
+        var messageInputHeight = messageInput.outerHeight();
+
+        var maxHeight = windowHeight - (navbarHeight + chatTitleHeight + navTabsHeight + messageInputHeight + 50);
+        messages.css("max-height", maxHeight+"px");
+    }
+
+    //If the user resizes the screen the height is updated
+    $(window).on('resize', function() { setContainerHeight(); });
+
+    //Call the function once
+    setContainerHeight();
+
+    // Returns a readable string representing a Date object (includes time of day).
+    function getFormattedDate(dt) {
+        var d = new Date(dt);
+        return d.toLocaleTimeString() + " on " + d.toLocaleDateString();
+
+    }
+
+    //This function receives the title and value of a metadata,
+    //creates a new panel and attaches it to the information tab
+    function displayMetadataInformation(title, value){
+
+        var divRow;
+        var lastRow = tabInformation.children(".row").last(); //Get the last row of information tab
+        var childrenNumber = lastRow.children().length; //Get the number of columns of that row
+
+        if (childrenNumber === 0 || childrenNumber === 3) { //If the row does not exist or it has 3 children
+            divRow = $("<div>", {class: "row"}); //Create a new row
+        } else {
+            divRow = lastRow; //or use the last row
+        }
+
+        //Elements to append the metadata information
+        var panelInformation = $("<div>", {class: "panel panel-default panel-information"});
+        var panelHeading = $("<div>", {class: "panel-heading"});
+        var panelBody = $("<div>", {class: "panel-body"});
+        var divColumn = $("<div>", {class: "col-sm-4"});
+
+        //Fill the content into the panel and add it to the row
+        panelHeading.html("<strong>" + title + "</strong>");
+        panelBody.html(value);
+        panelInformation.append(panelHeading);
+        panelInformation.append(panelBody);
+        divColumn.append(panelInformation);
+        divRow.append(divColumn);
+        tabInformation.append(divRow);
+
+    }
+
+    function getMetadataInformation(chatId){
+        $.getJSON("/api/v1/ticket/" + CHAT_ID + "/")
+            .success(function(data){
+
+                tabInformation.html("");
+
+                var metadataObject = data;
+                var dateCreated = metadataObject.created;
+                var dateClosed = metadataObject.closed;
+                var cost = metadataObject.cost;
+                var dueDate = metadataObject.due_date;
+                var notes = metadataObject.notes;
+
+                displayMetadataInformation("Date created", getFormattedDate(dateCreated));
+
+                if (dateClosed !== null) {
+                    displayMetadataInformation("Date closed", getFormattedDate(dateClosed));
+                }
+
+                if(cost != null){
+                    displayMetadataInformation("Cost", cost);
+                }
+
+                if (dueDate != null){
+                    displayMetadataInformation("Due Date", getFormattedDate(dueDate));
+                }
+
+                if(notes != null){
+                    displayMetadataInformation("Notes", notes);
+                }
+
+            });
+    }
+
+    function addMessage(object) {
+        var child = object.val();
+
+        chatParticipants.startAt().endAt().on("value", function(snapshot) {
+            var participants = snapshot.val();
+            var found = false;
+            for (var key in participants) {
+                if (participants.hasOwnProperty(key)) {
+                    var user = participants[key];
+                    if (CURRENT_USER == user) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found) {
+                chatParticipants.push(CURRENT_USER);
+            }
+
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+
+        //Create a new date field to use in Mustache
+        child.formattedDate = getFormattedDate(child.dt);
+
+        var messagesTemplate =
+            '<div class="row">' +
+                '<div class="col-xs-1">' +
+                '<div class="user-box pull-right">' +
+                    '<span class="text-muted">' +
+                        '<em>' +
+                        '{{ user }}' +
+                        '</em>' +
+                    '</span>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-xs-11">';
+
+        if(CURRENT_USER == child.user){
+            messagesTemplate += '<div class="message-container triangle-right right">';
+        }else{
+            messagesTemplate += '<div class="message-container triangle-right left">';
+        }
+        messagesTemplate +=
+                    '<p class="lead message-text">{{ desc }}</p> '+
+                    '<p class="message-date">{{ formattedDate }}</p>' +
+                    '</div>' +
+                '</div>' +
+            '</div>';
+
+        var renderedTemplate = Mustache.to_html(messagesTemplate, child);
+        messages.append(renderedTemplate);
+
+
+        // On new message load, scroll to the top.
+        messages[0].scrollTop = messages[0].scrollHeight;
+    }
+
+
+    if (typeof CHAT_ID != 'undefined' && typeof PROJECT_ID != 'undefined' &&
+            typeof CURRENT_USER_ID != 'undefined') {
+        // Initialise the Firebase
+        var ref = new Firebase("https://torid-fire-4899.firebaseio.com/");
+
+        // Creating a chat object
+        var projectObj = ref.child('project/' + PROJECT_ID);
+        var chatObj = projectObj.child('chats/' + CHAT_ID);
+
+        var chatParticipants = chatObj.child("participants");
+
+
+        var messagesRef = chatObj.child("messages");
+
+        //Listen for ENTER press and update Firebase
+        messageInput.keypress(function (e) {
+            if (e.keyCode == 13) {
+                messagesRef.push({
+                    desc: messageInput.val(),
+                    user: CURRENT_USER,
+                    user_id: CURRENT_USER_ID,
+                    dt: Date.now()
+                });
+                messageInput.val("");
+            }
+        });
+
+        messagesRef.on("child_added", function (object) {
+            addMessage(object)
+        });
+
+        getMetadataInformation(CHAT_ID);
+
+    }
+
+    $.getJSON("/api/v1/metadata_name/")
+        .success(function(metadataName){
+            var metadataObjects = metadataName.objects;
+            var metadataNameListTemplate = "{{#metadataName}}<li><a id='metadata-name-{{ id }}'>{{ name }}</a></li>{{/metadataName}}";
+            var renderedTemplate = Mustache.to_html(metadataNameListTemplate, {'metadataName' : metadataObjects});
+            $("#list-metadata-name")
+                .html(renderedTemplate)
+                .find("a")
+                .on("click", function(){
+                    $("#dropdown-metadata-name").html(this.text + ' <span class="caret"></span>');
+                    selectMetadataName = this.id.split("-")[2]; //Get the id of the selected metadata name
+                });
+        });
+
+    $("#confirm-add-note").on("click", function(){
+
+        var passData = {
+            "notes" : $("#note-value").val()
+        };
+
+        $.ajax({
+            url: apiCall + "ticket/" + CHAT_ID + "/",
+            type: "PUT",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(passData),
+            complete: function(){
+                getMetadataInformation(CHAT_ID);
+                $("#open-tab-information").tab("show");
+            }
+        });
+
+    });
+
+    //Add due date
+    $("#confirm-add-due-date").on("click", function(){
+
+        var passData = {
+            "due_date" : $("#due-date-value").val()
+        };
+
+        $.ajax({
+            url: apiCall + "ticket/" + CHAT_ID + "/",
+            type: "PUT",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(passData),
+            complete: function(){
+                getMetadataInformation(CHAT_ID);
+                $("#open-tab-information").tab("show");
+            }
+        });
+
+    });
+
+    $("#confirm-add-cost").on("click", function(){
+
+        var passData = {
+            "cost" : parseInt($("#cost-value").val())
+        };
+
+        $.ajax({
+            url: apiCall + "ticket/" + CHAT_ID + "/",
+            type: "PUT",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(passData),
+            complete: function(){
+                getMetadataInformation(CHAT_ID);
+                $("#open-tab-information").tab("show");
+            }
+        });
+
+    });
+
+});
---
+++ staticfiles/core/js/dashboard-sidebar.e23846d5744d.js
@@ -0,0 +1,125 @@
+$(function () {
+
+    //This function render the template using Mustache and substitute the element content
+    function renderTemplate(element, template, object){
+        var renderedTemplate = Mustache.to_html(template, object);
+        element.html(renderedTemplate);
+    }
+
+    /*
+    This function will update the max-height of the list to adapt to different screens
+    It is done by calculating the difference between the height of the window and the HTML elements
+    outerHeight is the height of element with its margins
+     */
+    function setListHeight() {
+        var windowHeight = $( window ).outerHeight();
+        var navbarHeight = $(".navbar-inverse").outerHeight();
+        var dropdownHeight = $("#dropdown-row").outerHeight();
+
+        var maxHeight = windowHeight - (navbarHeight + dropdownHeight + 50);
+        ticketsPanel.css("max-height", maxHeight+"px");
+    }
+
+    var ticketsPanel = $('#tickets-panel');
+    var openTicketsList = ticketsPanel.find('#open-tickets-list');
+    var closedTicketsList = ticketsPanel.find('#closed-tickets-list');
+    var navbarDropdownProjectList = $('#navbar-dropdown-list');
+    //Call the function once
+    setListHeight();
+
+    //If the user resizes the screen the height is updated
+    $(window).on('resize', function() { setListHeight(); });
+
+    var selected_project = null;  // Global variable so that we can access it for FireBase purposes later
+
+    //Disable the list button and the new chat button to avoid errors when there are no projects created
+    var sidebarDropdownButton = $('#sidebar-dropdown-button');
+    sidebarDropdownButton.prop('disabled', true);
+
+    var newChatButton = $('#new-project-button');
+    newChatButton.prop('disabled', true);
+
+
+    $.getJSON("/api/v1/project/")
+        .success(function (projects) {
+
+            //select first project
+            var projectObjects = projects.objects;
+
+            if (projectObjects.length > 0) {
+
+                sidebarDropdownButton.prop('disabled', false);
+
+                if(typeof PROJECT_ID != 'undefined'){
+                    selectProject(PROJECT_ID);
+                }else{
+                    selected_project = projectObjects[0].id;
+                    selectProject(selected_project);
+                }
+
+
+                //Render template. Update display
+                var projectsListTemplate = '{{#projects}}<li role="presentation"><a role="menuitem" tabindex="-1" href="#" id="{{id}}">{{name}}</a></li>{{/projects}}';
+                renderTemplate(navbarDropdownProjectList, projectsListTemplate, {'projects': projectObjects});
+
+                var newProjectLink = $("<li class='divider'></li><li><a href='/newproject'><span class='glyphicon glyphicon-plus'></span> New Project </a></li>");
+                navbarDropdownProjectList.append(newProjectLink);
+                //Bind the click event into projects items
+                navbarDropdownProjectList.find('li a').bind('click', function () {
+                    selected_project = this.getAttribute('id');
+                    selectProject(selected_project);
+                });
+            }
+
+
+        });
+
+    function selectProject(id) {
+        // Make ajax request
+
+        $.getJSON("/api/v1/chat/", {'project__id': id})
+            .success(function (chats) {
+
+                //Update the reference to the correct project id when creating a new chat and enable the button again
+                newChatButton.parent().attr('href', '/projects/' + id + '/newchat/');
+                newChatButton.prop('disabled', false);
+
+                //Separate open tickets from closed tickets and add them to the correct array
+                var chatObjects = chats.objects;
+                var openChatsObject = [];
+                var closedChatsObject = [];
+                for(var i in chatObjects){
+                    var project = chatObjects[i];
+                    if(project.closed == null){
+                        openChatsObject.push(project);
+                    } else {
+                        closedChatsObject.push(project);
+                    }
+                }
+
+                // Create mustache template for rendering tickets list
+				var chatListTemplate = '{{#chats}}<a class="list-group-item" id="chat-{{ id }}" href="/chats/{{ id }}">{{ title }}</a>{{/chats}}';
+                renderTemplate(openTicketsList, chatListTemplate, {'chats': openChatsObject});
+                renderTemplate(closedTicketsList, chatListTemplate, {'chats': closedChatsObject});
+
+
+                // Update the project name in the button
+                var project = $('#' + id);
+                var projectTitle = project.text();
+                var url = "/projects/"+id+"/info/";
+
+                if(typeof CHAT_ID != 'undefined'){
+                    url = url + "?next="+CHAT_ID;
+                }
+
+                $("#project-title").find("a").html(projectTitle).attr("href", url);
+
+                //Add a class active and make the accordion open
+                if (typeof CHAT_ID != 'undefined') {
+                    $("#chat-" + CHAT_ID).addClass('active').parent().parent().collapse('show');
+                }
+            });
+    }
+
+
+});
