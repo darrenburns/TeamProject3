3267815a456eee70bd0ef390e3ffd737de45e069
Gustavo Almansa Rodrigues <gustavo.almansa@gmail.com>
2015-07-25 21:14:22 +0100

    Added number of notifications on dropdown

--- core/static/core/js/NotificationCentre.react.js
+++ core/static/core/js/NotificationCentre.react.js
@@ -63,8 +63,7 @@ var NotificationCentre = React.createClass({
                 <a href="#" className="dropdown-toggle"
                    data-toggle="dropdown" role="button"
                    aria-haspopup="true" aria-expanded="false">
-                    Notifications
-                    <span className="caret"></span>
+                    <span class="badge">{ listOfNotifications.length }</span> Notifications <span className="caret"></span>
                 </a>
 
                 <ul className="dropdown-menu" aria-labelledby="dropdownMenuDivider">
